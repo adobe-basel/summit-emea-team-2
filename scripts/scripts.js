c0b54ad03d42362f495e538bcc92b3a4b774ddb8
José Correia <37300462+jose-correia@users.noreply.github.com>
2023-05-10 17:59:15 +0200

    Fix the favicon (#56)
    Co-authored-by: josec <josec@adobe.com>

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -105,7 +105,7 @@ async function loadLazy(doc) {
   loadFooter(doc.querySelector('footer'));
 
   loadCSS(`${window.hlx.codeBasePath}/styles/lazy-styles.css`);
-  addFavIcon(`${window.hlx.codeBasePath}/styles/favicon.svg`);
+  addFavIcon(`${window.hlx.codeBasePath}/styles/favicon.ico`);
   sampleRUM('lazy');
   sampleRUM.observe(main.querySelectorAll('div[data-block-name]'));
   sampleRUM.observe(main.querySelectorAll('picture > img'));
